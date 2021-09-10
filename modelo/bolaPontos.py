6caed87816a87fd490251f80a3f12ddb5d16d31a
arthurwinck <apwinck@gmail.com>
2021-09-10 11:41:14 -0300

    Implementacao Bola, BolaBranca e BolaPontos

---
+++ modelo/bolaPontos.py
@@ -0,0 +1,13 @@
+import pygame as py
+import pymunk
+
+from bola import Bola
+
+class BolaPontos(Bola):
+    def __init__(self, x,y, velocidade,superficie, valor):
+        super.__init__(self, x, y, velocidade, superficie)
+        self.valor = valor
+
+    def getValor(self):
+        return self.valor
+    
